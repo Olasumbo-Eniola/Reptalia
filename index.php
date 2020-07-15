845a46300fc334e290ec3bcb0b050c970e18a52b
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-07-15 23:41:33 +0100

    ....

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
-<?php
- header("Location: index.html");
-$servername = "localhost";
-$username = "id14342607_root";
-$password = "kafilat12*Sumbo";
-$dbname ="id14342607_mydb";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO getemail (email)
-  VALUES ('".$_POST["user_email"]."')";
-  // use exec() because no results are returned
-  $conn->exec($sql);
- 
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
-}
-$conn = null;
- ?>
-   
+<?php
+ob_start(); header('Location: index.html'); ob_end_flush();
+$servername = "localhost";
+$username = "id14342607_getmail";
+$password = "task12*Data12";
+$database = "id14342607_useremail";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $sql = "INSERT INTO email_data (email)
+  VALUES ('".$_POST["user_email"]."')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
