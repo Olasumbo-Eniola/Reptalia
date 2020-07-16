91125119dfbd7ec0516c32c461dfd0349a433873
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-07-17 00:46:47 +0100

    FF

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
     new WOW().init();
   });
+  function validEmail(){
+    let outputs = [ 'email'];
+    outputs[0] = document.forms['form']['user_email'].value;
+    var mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+    if (!outputs[0].match(mail)){
+      document.getElementsByTagName('small')[0].style.display = 'block';
+      return false;
+    }
+   
+  }
